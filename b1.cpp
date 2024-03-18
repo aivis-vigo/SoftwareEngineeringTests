2de931ecda279e4bcc3d0dca3087922d9ad41f15
aivis-vigo <aivisvigoreimarts@gmail.com>
2024-03-18 17:42:20 +0200

    Tasks B for from sample test

---
+++ b1.cpp
@@ -0,0 +1,47 @@
+/*
+ * B1[5]. Function countExtraAdjacentSpaces, which counts excessive spaces (any
+ * sequence of spaces of length n contains n-1 excessive spaces). E.g. (* denotes
+ * space), “alpha***beta**gamma” → 3
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int countExtraAdjacentSpaces(string& line) {
+    int extra_spaces = 0;
+    int consecutive_spaces = 0;
+
+    for (char character : line) {
+        if (character == ' ') {
+            consecutive_spaces++;
+        } else {
+            extra_spaces += max(0, consecutive_spaces - 1);
+            consecutive_spaces = 0;
+        }
+    }
+
+    return extra_spaces;
+}
+
+int mains() {
+    ifstream input_file;
+    string input_path = "/Users/Aivis/CLionProjects/SoftwareEngineeringTests/with_spaces.txt";
+    string current_line;
+
+    input_file.open(input_path);
+    getline(input_file, current_line);
+
+    while (input_file) {
+        int extra_spaces = countExtraAdjacentSpaces(current_line);
+        cout << "Adjacent spaces in line: " << extra_spaces << endl;
+
+        getline(input_file, current_line);
+    }
+
+    input_file.close();
+
+    return 0;
+}
---
+++ b2.cpp
@@ -0,0 +1,57 @@
+/*
+ * B2[9]. Function removeExtraAdjacentSpaces, which rewrites the text of f to another file
+ * the way all extra adjacent spaces are removed (any sequence of spaces is replaced by
+ * one space). E.g. (* denotes space) “*alpha*beta***gamma*******delta” →
+ * “*alpha*beta*gamma*delta”
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string removeExtraAdjacentSpace(string& line) {
+    string result;
+    bool is_space = false;
+
+    for (char character: line) {
+        if (character == ' ') {
+            if (!is_space) {
+                result += character;
+                is_space = true;
+            }
+        } else {
+            result += character;
+            is_space = false;
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    ifstream input_file;
+    ofstream output_file;
+    string input_path = "/Users/Aivis/CLionProjects/SoftwareEngineeringTests/with_spaces.txt";
+    string output_path = "/Users/Aivis/CLionProjects/SoftwareEngineeringTests/without_spaces.txt";
+    string current_line;
+
+    input_file.open(input_path);
+    output_file.open(output_path);
+
+    getline(input_file, current_line);
+
+    while (input_file) {
+        string processed_line = removeExtraAdjacentSpace(current_line);
+
+        output_file << processed_line << endl;
+
+        getline(input_file, current_line);
+    }
+
+    input_file.close();
+    output_file.close();
+
+    return 0;
+}
